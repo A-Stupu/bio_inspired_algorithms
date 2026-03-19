77fab88cf8d2c40daea692319542c608df3a8edf
lubinbenoit <lubin.benoit@gmail.com>
2026-03-19 15:10:26 +0100

    add: simple tour class + delta 2 opt function in tsp.py

--- tsp_ls/src/tsp.py
+++ tsp_ls/src/tsp.py
@@ -26,3 +26,45 @@ def parse_tsplib(filepath):
     # Build ordered list (0-indexed internally)
     nodes = [coords[i] for i in sorted(coords.keys())]
     return nodes, edge_weight_type
+
+class Tour:
+    def __init__(self, vertices):
+        self.vertices = vertices # list of tour vertex indices
+
+    def cost(self, distance_matrix):
+        """Calculate the total cost of the tour."""
+        total = 0.0
+        n = len(self.vertices)
+        for i in range(n):
+            a = self.vertices
+            b = self.vertices[(i + 1) % n]
+            total += distance_matrix[a][b]
+        return total
+    def distances(self, a, b, distance_matrix):
+        return distance_matrix[a][b] # distance between 2 vertices
+
+def delta_2opt(tour, dist, i, j):
+    """
+    Compute the delta (cost change) for a 2-opt swap 
+    between edges (i,i+1) and (j,j+1)
+    Args:
+        tour: list of vertex indices
+        dist: distance_matrix
+        i, j: indices
+    Returns:
+        float: delta (if negative -> improvement)
+    """
+    n = len(tour)
+    a = tour[i - 1]
+    b = tour[i]
+    c = tour[j]
+    d = tour[(j + 1) % n]
+
+    # Current cost: distance(a,b) + distance(c,d)
+    # Cost after swap: distance(a,c) + distance(b,d)
+    return (
+        - dist[a][b]
+        - dist[c][d]
+        + dist[a][c]
+        + dist[b][d]
+    )
