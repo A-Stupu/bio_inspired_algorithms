12ea0e04efb92363107cb76777e369be2f23336c
lubinbenoit <lubin.benoit@gmail.com>
2026-03-19 15:09:21 +0100

    add: help note in algorithms.py

--- tsp_ls/src/algorithms.py
+++ tsp_ls/src/algorithms.py
@@ -11,4 +11,46 @@
 # 8: end while
 # 9: return x
 
+from .operators import vertex_switching, two_opt
 
+
+
+def greedy_local_search():
+    return None
+
+
+# Naive approach :
+# neighbor = two_opt(current)
+# n_len = tour_length(neighbor, dist)
+
+# optimized approach (with delta 2 opt) :
+
+# for i in range(n - 1):
+#     for j in range(i + 2, n):
+
+#         if i == 0 and j == n - 1:
+#             continue
+
+#         delta = delta_2opt(current, dist, i, j)
+
+#         if delta < 0:
+#             apply_2opt(current, i, j)
+#             current_len += delta
+#             break
+
+
+def simulated_annealing():
+    return None
+
+
+
+
+
+# optimized approach :
+# i, j = sorted(random.sample(range(n), 2))
+
+# delta = delta_2opt(current, dist, i, j)
+
+# if delta < 0 or random.random() < math.exp(-delta / temp):
+#     apply_2opt(current, i, j)
+#     current_len += delta
